f7ba43c572c56ce50993e8be694bfb2062f0c942
jaycosnett <jay.cosnett@ohs.org>
2017-03-09 13:45:32 -0800

    First draft of Jay's OHS-branded version; includes changes discussed 3/8/17.

--- arArchivesOrPlugin/modules/staticpage/templates/homeSuccess.php
+++ arArchivesOrPlugin/modules/staticpage/templates/homeSuccess.php
@@ -1,10 +1,11 @@
 </div>
-<div id="homepage-hero" class="row">
+<div id="homepage-hero" class="row" title="View of Gresham and Mt. Hood from Mt. Tabor, Portland - ca.1905.
+Kiser Photo Collection, Org Lot 140, Box 4, Folder 41.">
   <div class="container">
   <?php $cacheKey = 'homepage-nav-'.$sf_user->getCulture() ?>
   <?php if (!cache($cacheKey)): ?>
     <div class="span3" id="homepage-nav">
-      <p><?php echo __('Browse by') ?></p>
+      <h2><?php echo __('Browse by') ?></h2>
       <ul>
         <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
         <?php if ($browseMenu->hasChildren()): ?>
@@ -21,15 +22,16 @@
     <?php cache_save($cacheKey) ?>
   <?php endif; ?>
 
+
+<div id="welcome-message__inner">
+	<h1 id="welcome-message">Oregon Historical Society Digital Collections</h1>
+</div>
+
+
  <div class="span4" id="intro">
 
-     <h2>
-        Oregon Historical Society <br/>
-		Digital Collections
-	</h2>
-	<p>
-    The Oregon Historical Society (OHS) - Davies Family Research Library is committed to providing broad access to our collections for teaching, learning, and research. The OHS Websites, catalog records, finding aids, and digital images enhance scholarship and promote use of both digital and physical objects.
-	<br /> <br />A critical component of our organizational mission is to “inspire curiosity about all the people, places, and events that have shaped Oregon” and providing free and open access to our collections is key to executing that mission. </p>
+	<h3>Providing Broad Access</h3>
+    <p>The Oregon Historical Society is committed to providing broad access to our collections for teaching, learning, and research. Our websites, catalog records, finding aids, and digital images enhance scholarship and promote use of both digital and physical objects. A critical component of our mission is to “inspire curiosity about all the people, places, and events that have shaped Oregon,” and providing free and open access to our collections is key to executing that mission.</p>
   </div>
 
 </div>
@@ -37,11 +39,11 @@
 <div class="container">
 <div id="homepage" class="row">
 
-  <div class="span4">
+  <div class="span7">
     <?php echo get_component('default', 'popular', array('limit' => 10, 'sf_cache_key' => $sf_user->getCulture())) ?>
   </div>
   <div class="span8" id="virtual-exhibit">
-    <a href="http://172.30.200.90/reuniting-finley-and-bohlman">
+    <a href="/reuniting-finley-and-bohlman">
       <h3>
         <?php echo __('Partnership') ?>
         <span class="title">Reuniting Finley and Bohlman</span>
@@ -49,7 +51,14 @@
       </h3>
       <div>&nbsp;</div>
     </a>
-	</div>
+  </div>
+    <div class="span4">
+  	    <section id="cta">
+            <h1 class="module__hed"><a href="http://ohs.org/research-and-library/" target="_blank">Visit Our Research Library</a></h1>
+            <a class="more-link--large" href="http://ohs.org/research-and-library/" target="_blank"><!--[if gte IE 9]><!--><svg viewBox='0 0 100 100' class='icon-chevron-right-large'><use xlink:href='#icon-chevron-right'></use></svg><!--<![endif]--></a>
+        </section>
+
+	  </div>
 </div>
 </div>
 <div class="container">
@@ -57,7 +66,7 @@
   <div class="slideshow-container">
 
   <div id="slide1"class="mySlides">
-    <a href="http://172.30.200.90/oregon-journal-photographic-negatives">
+    <a href="/oregon-journal-photographic-negatives">
     <h3>
 		<?php echo __('Featured Collection') ?>
 		<div class="title">Oregon Journal Photographic Negatives</div>
