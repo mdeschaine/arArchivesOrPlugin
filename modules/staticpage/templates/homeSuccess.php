c6356d4f1510900989b5b4b33dbfa567cb6f5b9f
MDeschaine <Mathieu.Deschaine@gmail.com>
2019-02-11 14:44:57 -0800

    mod

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -43,7 +43,7 @@ Kiser Photo Collection, Org Lot 140, Box 4, Folder 41.">
     <div class="span7" id:"module">
         <a class="mod-link" href="/informationobject/browse?sort=lastUpdated&view=card&onlyMedia=1&topLod=0">
           <div class="mod-img-holder" style="background-image:url(https://ohs.org/education/images/jesse-stahl-on-grave-digger-bc006600.jpg);"></div>
-        <div class="mod-over-img">See What's New</div>
+        <div class="mod mod-over-img">See What's New</div>
       </a>
     </div>
     <div class="span8" id="virtual-exhibit">
