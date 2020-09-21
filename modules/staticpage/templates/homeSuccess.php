01b20175bfb543a99fc1d0030fd6d2884a5963e5
Mathieu <mathieu.deschaine@gmail.com>
2020-09-21 11:24:09 -0700

    Update homeSuccess.php

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -204,7 +204,7 @@ Kiser Photo Collection, Org Lot 140, Box 4, Folder 41.">
                     </div>
                 </a>
                 <a class="partner-site" href="/the-immigrant-story">
-                    <div class="partner-site__img" style="background-image:url(https://theimmigrantstory.org/wp-content/uploads/2018/03/Headericonwhite-1.png);"></div>
+                    <div class="partner-site__img" style="background-image:url(/plugins/arArchivesOrPlugin/images/box3.jpg);"></div>
                     <div class="partner-site__text-holder">
                         <div class="partner-site__text">
                             <h1 class="partner-site__hed">The Immigrant Story</h1>
