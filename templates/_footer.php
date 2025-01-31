3e836b7a1344026b105864569a6b1be8830622d4
Mathieu <mathieu.deschaine@gmail.com>
2025-01-31 12:41:15 -0800

    Update _footer.php
    Swap GA tag from UA

--- templates/_footer.php
+++ templates/_footer.php
@@ -68,21 +68,15 @@
         <p class="foot__legal"><a class="foot-footer__link" href="http://www.ohs.org/contact.cfm" target="_blank">Contact OHS</a> <a class="foot-footer__link" href="http://www.ohs.org/privacy-policy.cfm" target="_blank">Privacy Policy</a> </p>
         <p class="foot__copyright">© Copyright 2017 Oregon Historical Society</p>
     </div>
-    <script>
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BK4MXDS94K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        ga('create', 'UA-65747041-2', 'auto');
-        ga('send', 'pageview');
+  gtag('config', 'G-BK4MXDS94K');
+</script>
 
-    </script>
 </footer>
